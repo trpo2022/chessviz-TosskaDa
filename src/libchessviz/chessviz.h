62b52f54c98be0564e9a5496956fdee9f9a67b28
Chahovskii Vlad <vladchahovckiy@mail.ru>
2022-03-23 20:23:27 +0700

    Delete .h

--- src/libchessviz/chessviz.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-void create(char mas[9][9]);
-void print(char mas[9][9]);
-void BlackMove(int* step_int, char mas[9][9]);
-void WhitePMove(int* step_int, char mas[9][9]);
-void WhiteTurn(char mas[9][9]);
-void BlackTurn(char mas[9][9]);
